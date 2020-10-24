52328cd95896fc0ef049682b9643ef15574928e4
discko <discko@qq.com>
2020-10-24 10:38:46 +0800

    disable security default login page

--- todolist-security/src/main/java/space/wudi/todolist/security/SecurityConfig.java
+++ todolist-security/src/main/java/space/wudi/todolist/security/SecurityConfig.java
@@ -1,9 +1,13 @@
 package space.wudi.todolist.security;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@EnableAutoConfiguration(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 @Configuration
 public class SecurityConfig {
     @Value("${my-variables.security.env}")
