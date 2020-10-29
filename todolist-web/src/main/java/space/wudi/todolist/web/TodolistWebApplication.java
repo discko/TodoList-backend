d71c25d9587ea8fe31cda09c3fb072c4ff9d06d4
discko <discko@qq.com>
2020-10-29 21:45:12 +0800

    test (#2)
    * add version api
    
    * edit version api
    
    * add lombok and slf4j.
    
    * add CORS setting

--- todolist-web/src/main/java/space/wudi/todolist/web/TodolistWebApplication.java
+++ todolist-web/src/main/java/space/wudi/todolist/web/TodolistWebApplication.java
@@ -5,7 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
-@SpringBootApplication(scanBasePackages = "space.wudi.todolist")
+@SpringBootApplication(scanBasePackages = {
+        "space.wudi.todolist.common",
+        "space.wudi.todolist.persisitance",
+        "space.wudi.todolist.service",
+        "space.wudi.todolist.security",
+        "space.wudi.todolist.web"
+})
 public class TodolistWebApplication {
 
     public static void main(String[] args) {
