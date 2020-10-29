32a18f307209a60f36695e739f60abc0b8c943fe
discko <discko@qq.com>
2020-10-29 21:45:12 +0800

    test (#2)
    * add version api
    
    * edit version api
    
    * add lombok and slf4j.
    
    * add CORS setting

--- todolist-web/src/main/java/space/wudi/todolist/web/controller/ServiceInfoController.java
+++ todolist-web/src/main/java/space/wudi/todolist/web/controller/ServiceInfoController.java
@@ -1,9 +1,10 @@
 package space.wudi.todolist.web.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.DependsOn;
-import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,25 +13,23 @@ import space.wudi.todolist.common.CommonConfig;
 import space.wudi.todolist.persisitance.PersistenceConfig;
 import space.wudi.todolist.security.SecurityConfig;
 import space.wudi.todolist.service.ServiceConfig;
-import space.wudi.todolist.web.TodolistWebApplication;
 import space.wudi.todolist.web.WebConfig;
 import space.wudi.todolist.web.vo.VoModuleVersionInfo;
 
 import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
 @RestController
-@RequestMapping(value="/ServiceInfo")
+@RequestMapping(value = "/ServiceInfo")
 public class ServiceInfoController {
 
     private static Map<String, VoModuleVersionInfo> versionInfo;
 
     @PostConstruct
-    void postConstruct(){
-        versionInfo=new HashMap<>();
+    void postConstruct() {
+        versionInfo = new HashMap<>();
     }
 
     @Bean
@@ -39,7 +38,7 @@ public class ServiceInfoController {
             "bindSecurityEnv",
             "bindPersistenceEnv",
             "bindCommonEnv"})
-    void bindVersionInfo(){
+    void bindVersionInfo() {
         versionInfo.put("web", new VoModuleVersionInfo(WebConfig.WEB_ENV, WebConfig.WEB_SERIAL));
         versionInfo.put("service", new VoModuleVersionInfo(ServiceConfig.SERVICE_ENV, ServiceConfig.SERVICE_SERIAL));
         versionInfo.put("security", new VoModuleVersionInfo(SecurityConfig.SECURITY_ENV, SecurityConfig.SECURITY_SERIAL));
@@ -48,43 +47,53 @@ public class ServiceInfoController {
         log.debug("create {}", versionInfo);
     }
 
-    @GetMapping(value="/version/{module}")
-    public VoModuleVersionInfo getModuleVersion(@PathVariable String module){
-        VoModuleVersionInfo mvi=_getModuleVersion(module);
-        return mvi;
+    @GetMapping(value = "/version/{module}")
+    @ApiOperation(value = "get module version by module name")
+    public VoModuleVersionInfo getModuleVersion(
+            @PathVariable
+            @ApiParam(value = "module name", allowableValues = "web,service,security,persistence,common", required = true)
+                    String module
+    ) {
+        return _getModuleVersion(module);
     }
 
-    private VoModuleVersionInfo _getModuleVersion(String module){
-        VoModuleVersionInfo mvi=versionInfo.getOrDefault(module, null);
+    private VoModuleVersionInfo _getModuleVersion(String module) {
+        VoModuleVersionInfo mvi = versionInfo.getOrDefault(module, null);
         log.debug("module {}: {}", module, mvi);
         return mvi;
     }
 
-    @GetMapping(value="/version/{module}/{part}")
+    @GetMapping(value = "/version/{module}/{part}")
+    @ApiOperation(value = "get module.part by specific module name and part name")
     public String getModuleVersionPart(
-            @PathVariable String module,
-            @PathVariable String part
-    ){
-        VoModuleVersionInfo mvi=_getModuleVersion(module);
-        if(mvi==null){
+            @PathVariable
+            @ApiParam(value = "module name", allowableValues = "web,service,security,persistence,common", required = true)
+                    String module,
+            @PathVariable
+            @ApiParam(value = "part name", allowableValues = "env,serial", required = true)
+                    String part
+    ) {
+        VoModuleVersionInfo mvi = _getModuleVersion(module);
+        if (mvi == null) {
             log.debug("module not found when get module.part");
             return null;
         }
-        String val=null;
-        switch(part.toLowerCase()){
+        String val = null;
+        switch (part.toLowerCase()) {
             case "env":
-                val=mvi.getEnv();
+                val = mvi.getEnv();
                 break;
             case "serial":
-                val=mvi.getSerial();
+                val = mvi.getSerial();
                 break;
         }
         log.debug("version part: {}.{}: {}", module, part, val);
         return val;
     }
 
-    @GetMapping(value="/version")
-    public Map<String, VoModuleVersionInfo> getVersion(){
+    @GetMapping(value = "/version")
+    @ApiOperation(value = "get full version info")
+    public Map<String, VoModuleVersionInfo> getVersion() {
         log.debug("print version info: {}", versionInfo);
         return versionInfo;
     }
