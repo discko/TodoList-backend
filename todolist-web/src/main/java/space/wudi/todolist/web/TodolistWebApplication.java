eeefdea18e502f23e53713099eb3ff328e5a177c
discko <discko@qq.com>
2020-10-30 21:40:12 +0800

    add enable auto confingure for each module.

--- todolist-web/src/main/java/space/wudi/todolist/web/TodolistWebApplication.java
+++ todolist-web/src/main/java/space/wudi/todolist/web/TodolistWebApplication.java
@@ -5,13 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
-@SpringBootApplication(scanBasePackages = {
-        "space.wudi.todolist.common",
-        "space.wudi.todolist.persisitance",
-        "space.wudi.todolist.service",
-        "space.wudi.todolist.security",
-        "space.wudi.todolist.web"
-})
+@SpringBootApplication
 public class TodolistWebApplication {
 
     public static void main(String[] args) {
