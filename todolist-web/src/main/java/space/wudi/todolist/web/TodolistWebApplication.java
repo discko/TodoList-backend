6cfc3b28c72f7b6963a083fc308ee3219a38bcae
discko <discko@qq.com>
2020-10-31 18:53:55 +0800

    fix idea "Could not autowire. No beans of 'UserService' type found."

--- todolist-web/src/main/java/space/wudi/todolist/web/TodolistWebApplication.java
+++ todolist-web/src/main/java/space/wudi/todolist/web/TodolistWebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "space.wudi.todolist")
 public class TodolistWebApplication {
 
     public static void main(String[] args) {
